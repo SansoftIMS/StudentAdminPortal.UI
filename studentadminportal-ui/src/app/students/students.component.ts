7476a9217780dd0b480ae8e97fe80f30abdc11df
sanch <sanchit@sansoftims.com>
2022-08-31 02:44:07 +0530

    Section-6

--- studentadminportal-ui/src/app/students/students.component.ts
+++ studentadminportal-ui/src/app/students/students.component.ts
@@ -5,16 +5,16 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Student } from '../models/ui-models/student.model';
 import { StudentService } from './student.service';
 
+
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
-
-  students:Student[] = [];
+  students: Student[] = [];
   displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email', 'mobile', 'gender', 'edit'];
-  dataSource:MatTableDataSource<Student> = new MatTableDataSource<Student>();
+  dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
   @ViewChild(MatPaginator) matPaginator!: MatPaginator;
   @ViewChild(MatSort) matSort!: MatSort;
   filterString = '';
@@ -42,7 +42,7 @@ export class StudentsComponent implements OnInit {
         }
       );
   }
-  
+
   filterStudents() {
     this.dataSource.filter = this.filterString.trim().toLowerCase();
   }
